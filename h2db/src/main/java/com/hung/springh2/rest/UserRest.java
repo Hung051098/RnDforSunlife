2aa18655ef7f86a7e2829c1c0c623a6fc6fa0f25
Hung051098 <39266629+Hung051098@users.noreply.github.com>
2021-08-18 19:22:08 +0700

    refresh config client

--- h2db/src/main/java/com/hung/springh2/rest/UserRest.java
+++ h2db/src/main/java/com/hung/springh2/rest/UserRest.java
@@ -2,19 +2,26 @@ package com.hung.springh2.rest;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 import com.hung.springh2.request.AddUserRequest;
 import com.hung.springh2.service.UserService;
 
+@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserRest {
 
+	@Value("${sample.example}")
+	private String example;
+	
 	@Autowired
 	UserService userService;
 
@@ -27,5 +34,13 @@ public class UserRest {
 	@PostMapping("/add_user")
 	public JSONObject addCustomer(@RequestBody AddUserRequest request) {
 		return userService.addCustomer(request);
+	}
+
+
+	@GetMapping("/getconfig")
+	public JSONObject getconfig( ) {
+		JSONObject data = new JSONObject();
+		data.put("data", example);
+		return data;
 	}
 }
--- service1/src/main/java/com/example/service1/rest/CustomerRest.java
+++ service1/src/main/java/com/example/service1/rest/CustomerRest.java
@@ -2,15 +2,20 @@ package com.example.service1.rest;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.RequestScope;
 
 import com.example.service1.service.CustomerService;
 
 @RestController
 @RequestMapping("/customer")
+@RequestScope
 public class CustomerRest {
+	@Value("${sample.example}")
+	String example;
 
 	@Autowired
 	CustomerService customerService;
@@ -23,5 +28,10 @@ public class CustomerRest {
 	@RequestMapping("/get_customer_by_id")
 	public JSONObject getCustomerById(@RequestParam int id ) {
 		return customerService.getCustomerById(id);
+	}
+
+	@RequestMapping("/getconfig")
+	public String getconfig( ) {
+		return example;
 	}
 }
