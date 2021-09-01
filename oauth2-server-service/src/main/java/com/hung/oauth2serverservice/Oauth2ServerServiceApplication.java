f28720d4af453c5fcf200b2aa605872bb97bb476
Hung051098 <39266629+Hung051098@users.noreply.github.com>
2021-09-01 18:29:39 +0700

    integration oauth2 server 0.2.0

---
+++ oauth2-server-service/src/main/java/com/hung/oauth2serverservice/Oauth2ServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hung.oauth2serverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ServerServiceApplication.class, args);
+	}
+
+}
---
+++ oauth2-server-service/src/test/java/com/hung/oauth2serverservice/Oauth2ServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hung.oauth2serverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
